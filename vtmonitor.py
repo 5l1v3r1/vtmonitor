7a4780a0439649ec46e55f7dc64e2835fb6afb16
Davide Tampellini <tampe125@gmail.com>
2017-05-23 08:22:49 +0200

    Added logging

--- vtmonitor.py
+++ vtmonitor.py
@@ -1,9 +1,8 @@
-import argparse
 import logging
+import logging.handlers
 import json
 import hashlib
 import subprocess
-from datetime import datetime
 from time import sleep
 from virus_total_apis import PublicApi as VirusTotalPublicApi
 from virus_total_apis import PrivateApi as VirusTotalPrivateApi
@@ -19,17 +18,31 @@ class VTMonitor:
         self.version = '1.0.0'
         self.api_key = None
         self.private_api = None
+        self.args = []
 
-        parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+        # Logging information
+        vt_logger = logging.getLogger('vtmonitor')
+        vt_logger.setLevel(logging.DEBUG)
 
-        self.args = parser.parse_args()
+        # Create a rotation logging, so we won't have and endless file
+        rotate = logging.handlers.RotatingFileHandler('vtmonitor.log', maxBytes=(5 * 1024 * 1024), backupCount=3)
+        rotate.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s|%(levelname)-8s| %(message)s', '%Y-%m-%d %H:%M:%S')
+        rotate.setFormatter(formatter)
+
+        vt_logger.addHandler(rotate)
+
+        console = logging.StreamHandler()
+        console.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s|%(levelname)-8s| %(message)s', '%Y-%m-%d %H:%M:%S')
+        console.setFormatter(formatter)
+        vt_logger.addHandler(console)
 
         # Let's silence the requests package logger
         logging.getLogger("requests").setLevel(logging.WARNING)
 
     def banner(self):
-        now = datetime.now()
-
         print("VTMonitor " + self.version + " - Watch and report")
         print("Copyright (C) 2017 FabbricaBinaria - Davide Tampellini")
         print("===============================================================================")
@@ -58,16 +71,22 @@ class VTMonitor:
         pass
 
     def run(self):
+        vt_logger = logging.getLogger('vtmonitor')
+
         self.banner()
 
         # Perform some sanity checks
         try:
             self.checkenv()
         except Exception as error:
-            print "[!] " + str(error)
+            vt_logger.critical(error)
             return
 
         self.check_updates()
+
+        vt_logger.debug('==================================================')
+        vt_logger.debug('Application restart')
+        vt_logger.debug('==================================================')
 
         if self.private_api:
             vt = VirusTotalPrivateApi(self.api_key)
@@ -75,7 +94,7 @@ class VTMonitor:
             vt = VirusTotalPublicApi(self.api_key)
 
         # Create baseline of processes
-        print "[*] Creating base list of allowed process"
+        vt_logger.info('Creating base list of allowed process')
         wmic = subprocess.check_output("wmic process get ExecutablePath", shell=True)
         wmic = wmic.replace('\r', '\n')
 
@@ -89,7 +108,7 @@ class VTMonitor:
 
             base_list.add(process)
 
-        print "[*] Starting main loop to watch for new processes"
+        vt_logger.info("Starting main loop to watch for new processes")
 
         while True:
             sleep(1)
@@ -106,7 +125,7 @@ class VTMonitor:
                 if process in base_list:
                     continue
 
-                print "[+] Unknown process %s, checking the hash on Virus Total" % process
+                vt_logger.debug("Unknown process %s, checking the hash on Virus Total" % process)
 
                 # New process, let's submit to VT for details
                 with open(process, 'rb') as handle:
@@ -115,21 +134,17 @@ class VTMonitor:
                 signature = hashlib.md5(data).hexdigest()
                 response = vt.get_file_report(signature)
 
-                msg = "[-] Process %s has a known signature on Virus Total" % process
-
                 if response['results'].get('response_code') == 0:
-                    msg = "[!] Process %s is unknown on Virus Total" % process
-
-                print msg
+                    vt_logger.warn("Process %s is unknown on Virus Total" % process)
+                else:
+                    vt_logger.info("Process %s has a known signature on Virus Total" % process)
 
                 # and add it to the base list, otherwise it will keep pinging VT all the time
                 base_list.add(process)
-
-                # print json.dumps(response, indent=4)
 
 try:
     scraper = VTMonitor()
     scraper.run()
 except KeyboardInterrupt:
     print("")
-    print ("[*] Operation aborted")
+    print ("Operation aborted")
